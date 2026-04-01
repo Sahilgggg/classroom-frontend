876055c9c07718a1bc8a68fb371a846eada61268
Sahilgggg <sahilgggfdcbu@gmail.com>
2026-04-02 01:10:26 +0530

    git add.

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,6 +3,9 @@ import { BACKEND_BASE_URL, USER_ROLES } from "../constants";
 
 export const authClient = createAuthClient({
   baseURL: "https://classroom-backend-production-72d4.up.railway.app/api/auth",
+  fetchOptions: {
+    credentials: "include", // 🔥 THIS IS THE FIX
+  },
   user: {
     additionalFields: {
       role: {

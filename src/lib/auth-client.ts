7f9f8ed3f5bc44bd49c46dc0207b83d3ff929526
Sahilgggg <sahilgggfdcbu@gmail.com>
2026-04-02 00:13:03 +0530

    some changes

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { BACKEND_BASE_URL, USER_ROLES } from "../constants";
 
 export const authClient = createAuthClient({
-  baseURL: `${BACKEND_BASE_URL}`,
+  baseURL: `${import.meta.env.VITE_BACKEND_BASE_URL}/auth`,
   user: {
     additionalFields: {
       role: {
